210fa5b0f877ec9f9eb0a567b6fb57367bab28a5
Kenneth Gorking <kgorking@outlook.com>
2022-08-25 11:11:01 +0200

    Fixed UB and refactor

--- include/poly_vector/poly_vector.h
+++ include/poly_vector/poly_vector.h
@@ -4,18 +4,18 @@
 #include <cstddef>
 #include <vector>
 
-#define PV_BASE(BASE) using pv_base = BASE; virtual void uninitialized_construct(pv_base* /*dst*/) = 0
-#define PV_IMPL() void uninitialized_construct(pv_base* dst) override { kg::detail::uninitialized_construct(dst, this); }
+#define PV_BASE(BASE) private: friend class kg::poly_vector<BASE>; virtual void pv_uninitialized_construct(BASE* /*dst*/) = 0
+#define PV_IMPL(BASE) private: friend class kg::poly_vector<BASE>; void pv_uninitialized_construct(BASE* dst) override { kg::detail::pv_uninitialized_construct(dst, this); }
 
 namespace kg {
 
 namespace detail {
     template<typename To, typename From>
-    constexpr void uninitialized_construct(To* dst, From *src) {
+    constexpr void pv_uninitialized_construct(To* dst, From *src) {
         if constexpr(std::move_constructible<From>) {
             std::construct_at<From, From&&>(static_cast<From*>(dst), std::move(*src));
         } else /*if constexpr(std::is_copy_constructible_v<To>)*/ {
-            static_assert(std::constructible_from<From, From&>, "type is not copy- or move-constructible");
+            static_assert(std::constructible_from<From, From const&>, "type is not copy- or move-constructible");
             std::construct_at<From, From const&>(static_cast<From*>(dst), *src);
         }
     }
@@ -25,11 +25,23 @@ template <typename Base>
 class poly_vector {
     static_assert(std::is_polymorphic_v<Base>, "The base class must be polymorphic");
     static_assert(std::has_virtual_destructor_v<Base>, "The base class must have a virtual destructor");
+    static_assert(requires { &Base::pv_uninitialized_construct; }, "Base does not have the required 'pv_uninitialized_construct' function. Add PV_BASE(class_name) to the end of it.");
 
 public:
+    ~poly_vector() {
+        destroy_all();
+    }
+
+    void clear() {
+        total_size = 0;
+        destroy_all();
+        bytes.clear();
+        offsets.clear();
+    }
+
     /*constexpr*/ Base& at(size_t const index) {
         auto const offset = offsets.at(index);
-        return *reinterpret_cast<Base*>(bytes.data() + offset);
+        return *std::launder(reinterpret_cast<Base*>(bytes.data() + offset));
     }
 
     template<typename T>
@@ -41,18 +53,18 @@ public:
             std::vector<std::byte> newbytes(vecsize);
 
             for(size_t const offset : offsets) {
-                auto src = reinterpret_cast<Base*>(bytes.data() + offset);
-                auto dst = reinterpret_cast<Base*>(newbytes.data() + offset);
+                auto src = std::launder(reinterpret_cast<Base*>(bytes.data() + offset));
+                auto dst = std::launder(reinterpret_cast<Base*>(newbytes.data() + offset));
 
-                src->uninitialized_construct(dst);
-                src->~Base();
+                src->pv_uninitialized_construct(dst);
+                std::destroy_at(src);
             }
 
             bytes = std::move(newbytes);
         }
         
-        Base* dest_t = reinterpret_cast<Base*>(bytes.data() + total_size);
-        detail::uninitialized_construct(dest_t, &t);
+        Base* dest_t = std::launder(reinterpret_cast<Base*>(bytes.data() + total_size));
+        detail::pv_uninitialized_construct(dest_t, &t);
         offsets.push_back(total_size);
         total_size += sizeof(T);
     }
@@ -61,8 +73,23 @@ public:
         return offsets.size();
     }
 
+    constexpr size_t size_at(size_t const index) const {
+        auto const offset = offsets.at(index);
+        if (index == offsets.size())
+            return total_size - offset;
+        else
+            return offsets.at(1 + index) - offset;
+    }
+
 private:
-    // lifted from msvc stl
+    void destroy_all() {
+        for(size_t const offset : offsets) {
+            auto src = std::launder(reinterpret_cast<Base*>(bytes.data() + offset));
+            std::destroy_at(src);
+        }
+    }
+
+    // lifted from msvc stl. 1.5x geometric growth
     constexpr size_t calc_growth(size_t const newsize) const {
         if (bytes.capacity() > (bytes.max_size() - bytes.capacity() / 2)) {
             return bytes.max_size();
--- unittest/unittest_poly_vector.cpp
+++ unittest/unittest_poly_vector.cpp
@@ -2,87 +2,114 @@
 #include "catch.hpp"
 #include <poly_vector/poly_vector.h>
 
-struct base_poly {
-	PV_BASE(base_poly);
+// A type to count constructions and destructions
+struct ctr_counter {
+	inline static size_t def_ctr_count = 0;
+	inline static size_t ctr_count = 0;
+	inline static size_t copy_count = 0;
+	inline static size_t move_count = 0;
+	inline static size_t dtr_count = 0;
 
-	virtual ~base_poly() = default;
+	ctr_counter() noexcept {
+		def_ctr_count++;
+		ctr_count++;
+	}
+	ctr_counter(ctr_counter const& /*other*/) {
+		copy_count++;
+		ctr_count++;
+	}
+	ctr_counter(ctr_counter&& /*other*/) noexcept {
+		move_count++;
+		ctr_count++;
+	}
+	~ctr_counter() {
+		dtr_count++;
+	}
+
+	ctr_counter& operator=(ctr_counter&&) = default;
+	ctr_counter& operator=(ctr_counter const&) = default;
+};
+
+struct base {
+	virtual ~base() = default;
 
 	virtual int run() {
 		return 0;
 	};
+
+	PV_BASE(base);
 };
 
-struct impl1_poly : base_poly {
-	PV_IMPL();
-
+struct impl1 : base {
 	int stuff = 1;
 
-	impl1_poly(int s) : stuff(s) {}
+	impl1(int s) : stuff(s) {}
 
 	int run() override {
 		return stuff;
 	};
+
+	PV_IMPL(base);
 };
 
 template <size_t N>
-struct impl2_poly : base_poly {
-	PV_IMPL();
-
+struct impl2 : base {
 	char some_more_stuff[N] = {};
 	int stuff = 99;
     std::vector<char> so_much_stuff;
 
-    impl2_poly(impl2_poly &&) = delete;
-    impl2_poly(impl2_poly const&) = default;
-	impl2_poly(int s) : stuff(s), so_much_stuff(s) {}
+	impl2(int s) : stuff(s), so_much_stuff(s) {}
 
 	int run() override {
 		return stuff;
 	};
+
+	PV_IMPL(base);
+};
+
+struct impl3 : base {
+	ctr_counter counter{};
+	PV_IMPL(base);
 };
 
 
 TEST_CASE("poly_vector specification") {
 	SECTION("A new poly_vector is empty") {
-        kg::poly_vector<base_poly> vec;
+        kg::poly_vector<base> vec;
         CHECK(0 == vec.size());
 	}
 
 	SECTION("An empty poly_vector") {
 		SECTION("grows when data is added to it") {
-            kg::poly_vector<base_poly> vec;
-            impl1_poly t{2};
-            vec.add(t);
+            kg::poly_vector<base> vec;
 
+            impl1 t{2};
+            vec.add(t);
             CHECK(1 == vec.size());
             CHECK(2 == vec.at(0).run());
+
+            impl1 u{3};
+            vec.add(u);
+            CHECK(2 == vec.size());
+            CHECK(3 == vec.at(1).run());
 		}
 	}
 
 	SECTION("Adding data to non-empty pv") {
-		SECTION("does not explode") {
-            kg::poly_vector<base_poly> vec;
-            vec.add(impl1_poly{2});
-            vec.add(impl1_poly{3});
-
-            CHECK(2 == vec.size());
-            CHECK(3 == vec.at(1).run());
-		}
-
 		SECTION("works with different types") {
-            kg::poly_vector<base_poly> vec;
+            kg::poly_vector<base> vec;
 
-            constexpr int num_values = 30;
+            constexpr int num_values = 56;
 
             for (int i = 0; i < num_values; ++i) {
-                if (i % 3)
-                    vec.add(impl2_poly<3*3>{i});
+                if (i % 7)
+                    vec.add(impl2<3*7>{i});
                 else if (i % 5)
-                    vec.add(impl2_poly<3*5>{i});
-                else if (i % 7)
-                    vec.add(impl2_poly<3*7>{i});
+                    vec.add(impl2<3*5>{i});
+                else if (i % 3)
+                    vec.add(impl2<3*3>{i});
                 else
-                    vec.add(impl1_poly{i});
+                    vec.add(impl1{i});
             }
 
             CHECK(num_values == vec.size());
@@ -92,4 +119,26 @@ TEST_CASE("poly_vector specification") {
             }
 		}
 	}
+
+	SECTION("data is destructed properly") {
+        {
+            kg::poly_vector<base> pv;
+            pv.add(impl3{});
+        }
+        CHECK(ctr_counter::ctr_count == 2); // construction + move
+        CHECK(ctr_counter::def_ctr_count == 1); // default construction
+        CHECK(ctr_counter::copy_count == 0);
+        CHECK(ctr_counter::move_count == 1); // internal move on resize
+        CHECK(ctr_counter::dtr_count == 2);
+
+        {
+            kg::poly_vector<base> pv;
+            pv.add(impl3{});
+        }
+        CHECK(ctr_counter::ctr_count == 4); // construction + move
+        CHECK(ctr_counter::def_ctr_count == 2); // default construction
+        CHECK(ctr_counter::copy_count == 0);
+        CHECK(ctr_counter::move_count == 2); // internal move on resize
+        CHECK(ctr_counter::dtr_count == 4);
+    }
 }
